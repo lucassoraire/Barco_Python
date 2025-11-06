5b489687b38420056bf3808a0c176a9949efcf1a
MAXX9s <maximo.cipriani@davinci.edu.ar>
2025-11-06 10:30:05 -0300

    csd

--- proyect_barc-coV6/run.py
+++ proyect_barc-coV6/run.py
@@ -1,8 +1,9 @@
 from app import create_app
-
+import os
 
 # ACA SE INICIA LA APP 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Render o 5000 por defecto
+    app.run(host="0.0.0.0", port=port)
